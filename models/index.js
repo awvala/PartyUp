d7410d493352cb0fa344a92977eaed1cf5d07610
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-08-27 18:26:53 -0500

    heroku fix

--- models/index.js
+++ models/index.js
@@ -1,30 +1,35 @@
 // Ray changed var config from starter template.
 "use strict";
- 
+
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 var db = {};
 
 fs
     .readdirSync(__dirname)
-    .filter(function(file) {
+    .filter(function (file) {
         return (file.indexOf(".") !== 0) && (file !== "index.js");
     })
-    .forEach(function(file) {
+    .forEach(function (file) {
         var model = sequelize.import(path.join(__dirname, file));
         db[model.name] = model;
     });
- 
-Object.keys(db).forEach(function(modelName) {
+
+Object.keys(db).forEach(function (modelName) {
     if ("associate" in db[modelName]) {
         db[modelName].associate(db);
     }
 });
- 
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
@@ -36,5 +41,5 @@ db.User.hasMany(db.Hosted_games);
 db.User.hasMany(db.Users_games);
 db.Users_games.belongsTo(db.User);
 db.Users_games.belongsTo(db.Hosted_games);
- 
+
 module.exports = db;
